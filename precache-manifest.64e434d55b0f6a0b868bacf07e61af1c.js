b3a695bcd080f4e31cf4d40128431526e3ef0198
Ankit Rathore <ankitrathore@Ankits-MacBook-Air.local>
2020-10-07 12:55:15 +0530

    Updates

---
+++ precache-manifest.64e434d55b0f6a0b868bacf07e61af1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03172d1fb6df9952d925c62b8f0f4d9e",
+    "url": "/To-Do-App-In-React/index.html"
+  },
+  {
+    "revision": "c05782c7c5f84656b2af",
+    "url": "/To-Do-App-In-React/static/css/main.85452444.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/To-Do-App-In-React/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App-In-React/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05782c7c5f84656b2af",
+    "url": "/To-Do-App-In-React/static/js/main.a2f0fd99.chunk.js"
+  },
+  {
+    "revision": "e91fbdeb1d555cd6eb88",
+    "url": "/To-Do-App-In-React/static/js/runtime-main.4c9d467b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App-In-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
